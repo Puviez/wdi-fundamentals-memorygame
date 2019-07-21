ea12d3b47fa59fe36abadd2a819c1712853783a2
Puvan <boredkeeper1793@gmail.com>
2019-07-21 11:52:15 +0800

    created functions for drawing and matching

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,21 +1,21 @@
-console.log("Up and Running!");
-
 var cards = ["Queen","Queen","King","King"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-var cardTwo = cards[2];
-//var cardThree = cards[2];
-//var cardFour = cards[3];
 
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardsInPlay[0]);
-console.log("User flipped " + cardsInPlay[1]);
-
-if (cardsInPlay.length === 2) {
+function checkForMatch() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert("You found a match!");
-	} else {
-		alert("Sorry try again.");
+			console.log("You found a match!");
+		} else {
+			console.log("Sorry try again.");
+		}
+}
+
+function flipCard(cardId) {
+	console.log(`User flipped ${cards[cardId]}`);
+	cardsInPlay.push(cards[cardId]);
+	if (cardsInPlay.length === 2) {
+		checkForMatch();
 	}
 }
+
+flipCard(0);
+flipCard(2);
